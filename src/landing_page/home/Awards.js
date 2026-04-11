6396ebd435d7101d2f9b740aa0298a0df7159629
Aniket <aniketraj8832@gmail.com>
2026-04-12 02:45:36 +0530

    feat: add initial home component UI

--- src/landing_page/home/Awards.js
+++ src/landing_page/home/Awards.js
@@ -1,7 +1,55 @@
 import React from "react";
 
 function Awards() {
-  return <h1>Awards</h1>;
+  return (
+    <div className="container mt-5">
+      <div className="row align items-center">
+        <div className="col-12 col-md-6 p-5 text-center">
+          <img src="media/images/largestBroker.svg" alt="broker" />
+        </div>
+        <div className="col-12 col-md-6 p-5 text-center text-md-start">
+          <h1>Largest stock broker in india</h1>
+          <p>
+            2+ million investify clients contribute to over 15% of all retail
+            order volume in india daily by trading and investing in:
+          </p>
+          <div className="row">
+            <div className="col-6">
+              <ul>
+                <li>
+                  <p>Futures and Options</p>
+                </li>
+                <li>
+                  <p>Commodity derivatives</p>
+                </li>
+                <li>
+                  <p>Currency derivatives</p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  <p>Stocks & IPOs</p>
+                </li>
+                <li>
+                  <p>Direct mutual funds</p>
+                </li>
+                <li>
+                  <p>Bonds and Govt. Securities</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <img
+            src="media/images/pressLogos.png"
+            style={{ width: "80%" }}
+            alt="logos"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Awards;
--- src/landing_page/home/Stats.js
+++ src/landing_page/home/Stats.js
@@ -1,7 +1,49 @@
 import React from "react";
 
 function Stats() {
-  return <h1>Stats</h1>;
+  return (
+    <div className="container mt-5">
+      <div className="row align-item-center ">
+        <div className="col-12 col-md-6 p-5 item-center">
+          <h2>Trust with confidence</h2>
+          <h4>Customer-first always</h4>
+          <p className="text-muted">
+            That's why 1.3+ crore customers trust Investify with{" "}
+            <i class="fa-solid fa-indian-rupee-sign fs-6"></i>3.5 lakh crores
+            worth of equity investments.
+          </p>
+          <h4>No spam or gimmicks</h4>
+          <p className="text-muted">
+            No gimmicks,spam,"gamification", or annoying push notifications.
+            High quality apps that you use at your pace, the way you like.
+          </p>
+          <h4>The Zerodha universe</h4>
+          <p className="text-muted">
+            Not just an app, but a whole ecosystem. Our investments in 30+
+            fintech startups offer you tailored services specific to your needs.
+          </p>
+          <h4>Do better with money</h4>
+          <p className="text-muted">
+            With initiatives like Nudge and kill Switch, we don't just
+            facilitate transactions, but actively help you do better with your
+            money.
+          </p>
+        </div>
+        <div className="col-12 col-md-6 p-5 text-center text-md-center">
+          <img src="media/images/ecosystem.png" width={"80%"} />
+          <div className="">
+            <a href="" className="me-5" style={{ textDecoration: "none" }}>
+              Explore our products{" "}
+              <i class="fa-solid fa-arrow-right" aria-hidden="true"></i>
+            </a>
+            <a href="" style={{ textDecoration: "none" }}>
+              Try Kite demo
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Stats;
