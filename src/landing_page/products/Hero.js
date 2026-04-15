78e8c006fe05000ea589d12c3ded11530dba7749
Aniket <aniketraj8832@gmail.com>
2026-04-15 23:47:00 +0530

    feat: implement hero section for products page

--- src/landing_page/products/Hero.js
+++ src/landing_page/products/Hero.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return ( 
-        <h1>
-            Hero
-        </h1>
-     );
+  return (
+    <div className="container">
+      <div className="row mt-5 p-5 ">
+        <div className="col text-center ">
+          <h1 className="fs-1">Technology</h1>
+          <h4 className="one-line text-muted">Sleek, modern, and intuitive trading platforms</h4>
+          <p>
+            Check out our <a href="" style={{textDecoration: "none"}}>investment offerings <i class="fa-solid fa-arrow-right" aria-hidden="true"></i></a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
