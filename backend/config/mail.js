13da15d444fe9b8eb488583681d904323cce2970
Aniket <aniketraj8832@gmail.com>
2026-06-03 09:03:22 +0530

    smtp verify test

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -11,4 +11,12 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP SERVER READY");
+  }
+});
+
 module.exports = transporter;
